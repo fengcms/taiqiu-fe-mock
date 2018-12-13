2ea8d36666a9c0e3ea8e8c7bd73e8fb9bdd2fd76
fungleo <web@fengcms.com>
2018-12-13 14:06:57 +0800

    新文件：   api/catalog.js

---
+++ api/catalog.js
@@ -0,0 +1,30 @@
+module.exports = {
+  list: {
+    get: {
+      status: 0,
+      data: {
+        list: [
+          {
+            id: 1,
+            name: '每日清台'
+          }, {
+            id: 2,
+            name: '台球练习'
+          }, {
+            id: 3,
+            name: '精彩比赛'
+          }, {
+            id: 4,
+            name: '大师赏析'
+          }, {
+            id: 5,
+            name: '台球资讯'
+          }, {
+            id: 2,
+            name: '联系我们'
+          }
+        ]
+      }
+    }
+  }
+}
